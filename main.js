0006a1932eef6931b3d2f57212bd8fa391ac6daa
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-06-19 17:54:43 -0700

    API added

--- main.js
+++ main.js
@@ -1,37 +1,106 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // Select tab content item
 function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab item
-	this.classList.add('tab-border');
-	// Grab content item from DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	// Add show class
-	tabContentItem.classList.add('show');
+  // Remove all show and border classes
+  removeBorder();
+  removeShow();
+  // Add border to current tab item
+  this.classList.add("tab-border");
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add("show");
 }
 
 // Remove bottom borders from all tab items
 function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
+  tabItems.forEach((item) => {
+    item.classList.remove("tab-border");
+  });
 }
 
 // Remove show class from all content items
 function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
 }
 
 // Listen for tab item click
-tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
+tabItems.forEach((item) => {
+  item.addEventListener("click", selectItem);
 });
 
+() => {
+  rafamed.dev;
+};
 
-() => { rafamed.dev }
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=63f9b8d50a434cd6bfef642737039f19&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=63f9b8d50a434cd6bfef642737039f19&query="';
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// Get initial movies
+getMovies(API_URL);
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
